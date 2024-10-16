1ac28a367a461312060b0aaa129af9231ba7b8c4
diptiranjansukla <diptiranjanshukla1998@gmail.com>
2024-10-16 21:16:52 +0530

    password

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     # Check if we are running in GitHub Actions
     if os.getenv('GITHUB_ACTIONS'):
-        SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password@localhost:3306/farmer_warehouse_db'
+        SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Sukla%40123@127.0.0.1:3306/farmer_warehouse_db'
     else:
         # Local development configuration
         SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Sukla%40123@localhost/farmer_warehouse_db'
